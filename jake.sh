e7516fc4b62b2f271857191a5c33a11972404517
lassehp <lhp@lhpl.dk>
2016-08-25 11:36:07 +0200

    what if build cannot complete before next scheduled poll...

--- jake.sh
+++ jake.sh
@@ -2,4 +2,4 @@
 echo "Mathemagically wrong spell!"
 echo "I wasn't scared! I was singing! I was singing my scream song. Wooaah! Wooaaooh!"
 echo "I love Lady Rainicorn"
-
+echo "this task is SLOOOOOOOOW!" ; sleep 80
