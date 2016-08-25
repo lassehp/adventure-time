35c8d3139be79ac012bc03bc5e3df592e25a0479
lassehp <lhp@lhpl.dk>
2016-08-25 15:21:58 +0200

    Magical would satisfy more

--- jake.sh
+++ jake.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-echo "Mathemagically incorrect spell!"
+echo "Mathemagically unsatisfactory spell!"
 echo "I wasn't scared! I was singing! I was singing my scream song. Wooaah! Wooaaooh!"
 echo "I love Lady Rainicorn"
 echo "this task is FAST"
