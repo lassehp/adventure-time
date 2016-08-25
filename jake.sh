2a7c1497e9912a12712b80bdb8de242093daf3fe
lassehp <lhp@lhpl.dk>
2016-08-25 11:28:30 +0200

    Ack the spells...

--- jake.sh
+++ jake.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-echo "Mathemagically wrong!"
+echo "Mathemagically wrong spell!"
 echo "I wasn't scared! I was singing! I was singing my scream song. Wooaah! Wooaaooh!"
 echo "I love Lady Rainicorn"
 
